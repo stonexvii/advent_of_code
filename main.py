0eb003f62ab8a2402460cb00c7163b72b8528958
STONE <17th@inbox.ru>
2025-07-23 23:17:23 +0300

    add init module

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+solution_file = '''
+def read_data(path: str):
+    with open(path, 'r') as file:
+        pass
+
+def solution(path: str):
+    pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    solution()
+'''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def create_file(path: str):
+    data = solution_file if path.endswith('.py') else ''
+    with open(path, 'w') as file:
+        file.write(data)
+
+
+def create_files(work_dir):
+    work_dir = str(work_dir)
+    if not os.path.exists(work_dir):
+        os.mkdir(work_dir)
+        for num in range(1, 26):
+            dir_name = f'day_{str(num).zfill(2)}'
+            path = os.path.join(work_dir, dir_name)
+            if not os.path.exists(path):
+                os.mkdir(path)
+                files = [
+                    'part_01.py',
+                    'part_02.py',
+                    'input_data.txt',
+                    'test_data.txt',
+                ]
+                files = map(lambda x: os.path.join(path, x), files)
+                for file in files:
+                    create_file(file)
+        print('Done!')
+    else:
+        print('Directory already exists')
+
+
+if __name__ == '__main__':
+    create_files(2016)
