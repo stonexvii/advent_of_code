dc475a2526efde23225a7bcabbd4224a36cd1818
STONE <17th@inbox.ru>
2025-07-26 00:01:22 +0300

    2016_09

--- 2016/day_09/part_01.py
+++ 2016/day_09/part_01.py
@@ -1,11 +1,29 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return file.read()
+
+
+def split_marker(item: str):
+    return tuple(map(int, item.split('x')))
+
 
 def solution(path: str):
-    pass
+    data = read_data(path)
+    uncompressed = ''
+    i = 0
+    while i < len(data):
+        print(uncompressed[-80:], data[i], i)
+        if data[i] != '(':
+            uncompressed += data[i]
+            i += 1
+        else:
+            idx_closed = data.index(')', i)
+            slc, cnt = split_marker(data[i + 1:idx_closed])
+            next_idx = idx_closed + 1
+            uncompressed += data[next_idx:next_idx + slc] * cnt
+            i = next_idx + slc
+    return len(uncompressed)
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
--- 2016/day_09/part_02.py
+++ 2016/day_09/part_02.py
@@ -1,11 +1,48 @@
+from string import ascii_uppercase
+from functools import reduce
+from operator import mul
+
 
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return file.read()
+
+
+def split_marker(item: str):
+    item = item.replace('(', ' ').replace(')', ' ').split()
+    item = tuple(map(lambda x: x.split('x'), item))
+    cnt = reduce(mul, map(lambda x: int(x[1]), item))
+    print(int(item[-1][0]), cnt)
+    return int(item[-1][0]), cnt
+
+
+def next_index(word: str):
+    print('word', word)
+    for i in range(len(word)):
+        if word[i].isupper():
+            print(i)
+            return i
+
 
 def solution(path: str):
-    pass
+    data = read_data(path)
+    uncompressed = ''
+    i = 0
+    while i < len(data):
+        print(uncompressed[-80:], data[i], i)
+        if data[i] != '(':
+            uncompressed += data[i]
+            i += 1
+        else:
+            idx_closed = i + next_index(data[i:]) - 1
+            slc, cnt = split_marker(data[i:idx_closed+1])
+            next_idx = idx_closed + 1
+            for _ in range(cnt):
+                uncompressed += data[next_idx:next_idx + slc]
+            i = next_idx + slc
+        print(uncompressed)
+    return len(uncompressed)
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('test_data.txt'))
