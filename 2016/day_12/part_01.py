e3175330b51089b93283229e626485f906990314
STONE <17th@inbox.ru>
2025-08-06 00:49:44 +0300

    2016_12

--- 2016/day_12/part_01.py
+++ 2016/day_12/part_01.py
@@ -1,11 +1,40 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        data = [x.strip().split() for x in file.readlines()]
+    return data
+
+
+class Game:
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+        self.c = 0
+        self.d = 0
+
+    def move(self, *args):
+        if args[0] == 'cpy':
+            self.__setattr__(args[2], int(args[1]) if args[1].isdigit() else self.__getattribute__(args[1]))
+        elif args[0] == 'inc':
+            self.__setattr__(args[1], self.__getattribute__(args[1]) + 1)
+        elif args[0] == 'dec':
+            self.__setattr__(args[1], self.__getattribute__(args[1]) - 1)
+        else:
+            if args[1].isdigit() and int(args[1]) or self.__getattribute__(args[1]):
+                return int(args[2])
+        return 1
+
+    def __str__(self):
+        return str(self.__dict__)
+
 
 def solution(path: str):
-    pass
+    data_map = read_data(path)
+    idx = 0
+    game = Game()
+    while idx < len(data_map):
+        idx += game.move(*data_map[idx])
+    return game
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
--- 2016/day_13/part_01.py
+++ 2016/day_13/part_01.py
@@ -1,11 +1,14 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
         pass
 
+
 def solution(path: str):
-    pass
+    for x in range(10):
+        for y in range(10):
+            print('.' if sum(map(int, bin(x * x + 3 * x + 2 * x * y + y + y * y)[2:])) % 2 else '#', end='')
+        print()
 
 
 if __name__ == '__main__':
-    solution()
+    solution('f')
