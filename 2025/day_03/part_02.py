30a5c4e5c9a83047e08aa25086b3c6baa8303ca1
STONE <17th@inbox.ru>
2025-12-03 23:48:55 +0300

    upd: 2025_3

--- 2025/day_03/part_02.py
+++ 2025/day_03/part_02.py
@@ -1,13 +1,16 @@
+MAX_CELLS = 12
+
+
 def read_data(path: str):
     with open(path, 'r') as file:
         return [list(map(int, line.strip())) for line in file.readlines()]
 
 
 def get_max(line: list[int]):
-    idx, k = 0, 11
+    idx, k = 0, MAX_CELLS - 1
     result = []
-    while len(result) < 12:
-        max_int = max(line[idx:len(line)-k])
+    while len(result) < MAX_CELLS:
+        max_int = max(line[idx:len(line) - k])
         result.append(max_int)
         idx = line.index(max_int, idx) + 1
         k -= 1
