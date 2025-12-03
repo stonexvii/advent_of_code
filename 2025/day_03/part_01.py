f3a0479fcdcfad4a52d00d4faec2184638bbdcb8
STONE <17th@inbox.ru>
2025-12-03 23:48:07 +0300

    upd: 2025_3

--- 2025/day_03/part_01.py
+++ 2025/day_03/part_01.py
@@ -1,11 +1,21 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return [list(map(int, line.strip())) for line in file.readlines()]
+
+
+def get_max(line: list[int]):
+    result = []
+    for i in range(len(line) - 1):
+        result.append(line[i]*10 + max(line[i + 1:]))
+    return result
+
 
 def solution(path: str):
-    pass
+    result = 0
+    for line in read_data(path):
+        result += max(get_max(line))
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
--- 2025/day_03/part_02.py
+++ 2025/day_03/part_02.py
@@ -1,11 +1,26 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return [list(map(int, line.strip())) for line in file.readlines()]
+
+
+def get_max(line: list[int]):
+    idx, k = 0, 11
+    result = []
+    while len(result) < 12:
+        max_int = max(line[idx:len(line)-k])
+        result.append(max_int)
+        idx = line.index(max_int, idx) + 1
+        k -= 1
+    return result
+
 
 def solution(path: str):
-    pass
+    result = 0
+    for line in read_data(path):
+        get_max(line)
+        result += int(''.join(list(map(str, get_max(line)))))
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
