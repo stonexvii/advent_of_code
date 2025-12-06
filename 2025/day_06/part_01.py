f8900a3bb3e8bf287274dc085722499264c8eb9e
STONE <17th@inbox.ru>
2025-12-06 18:49:03 +0300

    add: 2025_06

--- 2025/day_06/part_01.py
+++ 2025/day_06/part_01.py
@@ -1,11 +1,25 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return list(zip(*[line.strip().split() for line in file.readlines()]))
+
+
+def calculate(data_line: list[str]):
+    operator = data_line.pop()
+    result = 1 if operator == '*' else 0
+    for number in data_line:
+        if operator == '*':
+            result *= int(number)
+        else:
+            result += int(number)
+    return result
+
 
 def solution(path: str):
-    pass
+    result = 0
+    for line in read_data(path):
+        result += calculate(list(line))
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
--- 2025/day_06/part_02.py
+++ 2025/day_06/part_02.py
@@ -1,11 +1,35 @@
+from itertools import zip_longest
+
 
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return list(zip_longest(*[list(line.strip().replace(' ', '0')) for line in file.readlines()], fillvalue='0'))
+
+
+def calculate(data_line: list[str]):
+    operator = data_line.pop(0)
+    result = 1 if operator == '*' else 0
+    for number in data_line:
+        if operator == '*':
+            result *= number
+        else:
+            result += number
+    return result
+
 
 def solution(path: str):
-    pass
+    result, temp_data, operator = 0, [], None
+    for line in read_data(path):
+        if line[-1] != '0':
+            temp_data.append(line[-1])
+        if len(set(line)) != 1:
+            temp_data.append(int(''.join(line[:-1]).replace('0', '')))
+        else:
+            result += calculate(temp_data)
+            temp_data = []
+    result += calculate(temp_data)
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
