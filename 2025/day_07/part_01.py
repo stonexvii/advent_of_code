ef6d022855e9b03448ca7415b7e533e9dd110f0d
STONE <17th@inbox.ru>
2025-12-07 18:12:06 +0300

    add: 2025_07

--- 2025/day_07/part_01.py
+++ 2025/day_07/part_01.py
@@ -1,11 +1,22 @@
-
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        while line := file.readline():
+            yield line.strip()
+
 
 def solution(path: str):
-    pass
+    result, data_map = 0, read_data(path)
+    start_idx = next(data_map).index('S')
+    indexes = {0: {start_idx}}
+    for row_idx, line in enumerate(data_map, 1):
+        indexes[row_idx] = indexes[row_idx - 1].copy()
+        for idx in indexes[row_idx - 1]:
+            if line[idx] == '^':
+                indexes[row_idx].remove(idx)
+                indexes[row_idx].update({idx - 1, idx + 1})
+                result += 1
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
--- 2025/day_07/part_02.py
+++ 2025/day_07/part_02.py
@@ -1,11 +1,26 @@
+from functools import lru_cache
+
 
 def read_data(path: str):
     with open(path, 'r') as file:
-        pass
+        return [line.strip() for line in file.readlines()]
+
 
 def solution(path: str):
-    pass
+    data_map = read_data(path)
+    start_idx = data_map[0].index('S')
+
+    @lru_cache()
+    def split_ray(row_line: int, idx: int):
+        for row_idx, line in enumerate(data_map[row_line:], row_line):
+            if line[idx] == '^':
+                return split_ray(row_idx, idx - 1) + split_ray(row_idx, idx + 1)
+            if row_idx == len(data_map) - 1:
+                return 1
+
+    result = split_ray(0, start_idx)
+    return result
 
 
 if __name__ == '__main__':
-    solution()
+    print(solution('input_data.txt'))
